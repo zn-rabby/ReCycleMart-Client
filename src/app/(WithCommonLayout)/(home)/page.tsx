a77b034b2c2247f85d5b804bb974d9af4060a3ad
zn-rabby <rabby.webdeveloper@gmail.com>
2025-03-22 22:02:11 +0600

    FeaturesSection added

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -4,11 +4,13 @@ import CombinedSection from "@/components/modules/home/CombinedSection/CombinedS
 import FeaturedProducts from "@/components/modules/home/FeaturedProducts";
 import HeroSection from "@/components/modules/home/HeroSection";
 import CategorySection from "@/components/modules/home/HeroSection/Category";
+import FeaturesSection from "@/components/modules/home/HeroSection/FeaturesSection/FeaturesSection";
 
 const HomePage = async () => {
   return (
     <div>
       <HeroSection></HeroSection>
+      <FeaturesSection></FeaturesSection>
       <CategorySection></CategorySection>
       <FeaturedProducts></FeaturedProducts>
       <Brand></Brand>
